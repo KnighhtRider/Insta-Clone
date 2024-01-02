f1a99658fc44ab338bb99096dc7beea4220282a7
Vivek Thakur <110936458+KnighhtRider@users.noreply.github.com>
2024-01-02 13:01:59 +0530

    :construction: Signup page Modified

--- components/Signup.js
+++ components/Signup.js
@@ -65,20 +65,20 @@ function Signup() {
 
 
   return (
-    <div classNameName="h-screen flex flex-row justify-center items-center">
-      <div classNameName=" bg-gray-100 flex flex-col justify-center items-center lg:col-span-1">
-        <div classNameName="bg-white border w-80 pt-8 flex items-center flex-col mb-3">
-          <h1 class="bg-no-repeat instagram-logo"></h1>
+    <div className="h-screen flex flex-row justify-center items-center">
+      <div className=" bg-gray-100 flex flex-col justify-center items-center lg:col-span-1">
+        <div className="bg-white border w-80 pt-8 flex items-center flex-col mb-3">
+          <h1 className="bg-no-repeat instagram-logo"></h1>
           <p className='text-center text-base px-5 pt-5 font-semibold text-gray-500'>Sign up to see photos and videos from your friends.</p>
 
           <button className="mt-4 flex bg-blue-400 hover:bg-blue-500 text-white py-2 px-4 rounded w-64 justify-center">
-            <div classNameName="bg-no-repeat facebook-logo mr-1 mt-1"></div>
+            <div className="bg-no-repeat facebook-logo mr-1 mt-1"></div>
             <span className='font-normal'>
               Log in with Facebook
             </span>
           </button>
 
-          <div classNameName="flex justify-evenly space-x-2 w-64 mt-4">
+          <div className="flex justify-evenly space-x-2 w-64 mt-4">
             <span className="bg-gray-300 h-px flex-grow t-2 relative top-2"></span>
             <span className="flex-none uppercase text-xs text-gray-400 font-semibold">
               or
@@ -86,7 +86,7 @@ function Signup() {
             <span className="bg-gray-300 h-px flex-grow t-2 relative top-2"></span>
           </div>
 
-          <div classNameName="mt-8 w-64 flex flex-col">
+          <div className="mt-8 w-64 flex flex-col">
             <input
               autofocus
               className="text-xs w-full mb-2 rounded border bg-gray-100 border-gray-300 px-2 py-2 focus:outline-none focus:border-gray-400 active:outline-none"
@@ -134,13 +134,13 @@ function Signup() {
 
         </div>
         <div className="bg-white border border-gray-300 text-center w-80 py-4">
-          <span class="text-sm">Have an account?</span>
-          <a class="text-blue-500 text-sm font-semibold hover:text-blue-700 cursor-pointer"
+          <span className="text-sm">Have an account?</span>
+          <a className="text-blue-500 text-sm font-semibold hover:text-blue-700 cursor-pointer"
             onClick={() => router.push('/login')}
           > Log in</a>
         </div>
         <div className="mt-3 text-center">
-          <span class="text-xs">Get the app</span>
+          <span className="text-xs">Get the app</span>
           <div className="flex mt-3 space-x-2">
             <div className="bg-no-repeat apple-store-logo"></div>
             <div className="bg-no-repeat google-store-logo"></div>
